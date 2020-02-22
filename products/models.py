056b397775f2b34d8b3452ce2649c593ef43061e
Mwaiyusuf <mwaigalo5@gmail.com>
2020-02-22 23:42:20 +0300

    product medels

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+# hackers could give a long name
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_lenght=2083)
