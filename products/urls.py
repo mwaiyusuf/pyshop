b27058eabd28b24b9b11c1e312950d473cce3210
Mwaiyusuf <mwaigalo5@gmail.com>
2020-02-22 23:07:47 +0300

    adding a function

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 # we are importing the path function
 from django.urls import path
 # we av to import the viewa module
-from . import views
+from . import  views
 # , means the current folder
 # and the module ends up to be an object
 urlpatterns =[
