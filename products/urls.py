a4a5ed648f78ff6139e58789f39753f0749f0d4a
Mwaiyusuf <mwaigalo5@gmail.com>
2020-02-23 23:32:03 +0300

    path importation

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 # we av to import the viewa module
 from . import  views
-# , means the current folder
+# , mean s the current folder
 # and the module ends up to be an object
 urlpatterns =[
 # the quotes are left empty to show the root of the app in routing
