707a48d81483ec974ad042b839567101d3347132
Mwaiyusuf <mwaigalo5@gmail.com>
2020-02-24 22:09:02 +0300

    products stock

--- products/models.py
+++ products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
 
     name = models.CharField(max_length=255)
     price = models.FloatField()
-    stock = models.IntegerField()
+    # stock = models.IntegerField()
     image_url = models.CharField(max_lenght=2083)
