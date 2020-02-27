e05d8cadf25159c8a64140ffe0bc83ac3f16942d
Mwaiyusuf <mwaigalo5@gmail.com>
2020-02-27 23:33:33 +0300

    rendering the products and offers

--- products/views.py
+++ products/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 # from the django.http module we are importing the HttpResponse class
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Product
 # Create your views here.
 
 def index(request):
     products = Product.objects.all()
+# renders the request of data to be passed using the dictionary
+    return render(request, 'index.html', {'products':products})
  # we are creating an instance of this class HttpResponse
-    return HttpResponse('welcome to the first trial')
+ #    return HttpResponse('welcome to the first trial')
 # def new(request):
 #     return HttpResponse("arrival of new products")
