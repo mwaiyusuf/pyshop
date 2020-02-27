d161ad5f8a95edc71ff02bbaf34b83c534278dac
Mwaiyusuf <mwaigalo5@gmail.com>
2020-02-27 23:14:25 +0300

    displayin the products to the users

--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 # from the django.http module we are importing the HttpResponse class
 from django.http import HttpResponse
-
+from .models import Product
 # Create your views here.
 
 def index(request):
+    products = Product.objects.all()
  # we are creating an instance of this class HttpResponse
     return HttpResponse('welcome to the first trial')
 # def new(request):
