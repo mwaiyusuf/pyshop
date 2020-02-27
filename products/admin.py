9e4e3f83756c651bc19e02e645639cf3434bbfa7
Mwaiyusuf <mwaigalo5@gmail.com>
2020-02-27 23:01:27 +0300

    listing the offers

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Product, offer
 # Register your models here.
 
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ("code")
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name","price","stock")
 
