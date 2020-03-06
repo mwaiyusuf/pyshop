17fd6373f8b422f6665572f3627297ae03b0dd3f
Mwaiyusuf <mwaigalo5@gmail.com>
2020-03-06 22:56:31 +0300

    adding class for NewsLetterRecipitent

--- products/models.py
+++ products/models.py
@@ -14,3 +14,7 @@ class Offer(models.Model):
     code = models.CharField(max_length=10)
     description = models.CharField(max_length=255)
     discount = models.FloatField()
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
