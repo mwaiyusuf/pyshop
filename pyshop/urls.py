a81acd7dff63373c06ccae1401d22a8d57ff956f
Mwaiyusuf <mwaigalo5@gmail.com>
2020-03-10 23:20:11 +0300

    url pattern for registration

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 # Any time there is  url to the prodcuts sent them to the product app
     path('products/',include('products.urls'))
+    path('^',include('regsitatration.backends.simple.urls'))
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
